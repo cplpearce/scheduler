88a561997b682cacd759d7f017ad9a6a248950c2
cplpearce <clint.pearce@rocketmail.com>
2020-10-07 20:48:34 -0400

    Add screenshots

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import InterviewerListItem from 'components/InterviewerListItem';
-import PropTypes from 'prop-types';
-import 'components/InterviewerList.scss';
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from "prop-types";
+import "components/InterviewerList.scss";
 
+// I N T E R V I E W E R   L I S T   R E N D E R S
+// T H E   A V A I L A B L E   I N T E R V I E W E R S
 export default function InterviewerList(props) {
-  let interviewers = props.interviewers.map(interviewer => {
+  let interviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
-        setInterviewer={event => props.setInterviewer(interviewer.id)}
+        setInterviewer={(event) => props.setInterviewer(interviewer.id)}
       />
     );
   });
@@ -23,8 +25,8 @@ export default function InterviewerList(props) {
   );
 }
 
-// Set prop types
+// P R O P   T Y P E S   C H E C K I N G
 InterviewerList.propTypes = {
   interviewer: PropTypes.number,
-  setInterviewer: PropTypes.func.isRequired
+  setInterviewer: PropTypes.func.isRequired,
 };
